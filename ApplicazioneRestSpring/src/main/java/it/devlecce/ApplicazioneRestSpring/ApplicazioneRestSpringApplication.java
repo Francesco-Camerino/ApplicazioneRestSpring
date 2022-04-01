888d004f002de9a2a0f7d234925ca2766dfdea86
Francesco-Camerino <frcamerino@gmail.com>
2022-04-01 11:19:20 +0200

    Commit .java

---
+++ ApplicazioneRestSpring/src/main/java/it/devlecce/ApplicazioneRestSpring/ApplicazioneRestSpringApplication.java
@@ -0,0 +1,13 @@
+package it.devlecce.ApplicazioneRestSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicazioneRestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicazioneRestSpringApplication.class, args);
+	}
+
+}
---
+++ ApplicazioneRestSpring/src/test/java/it/devlecce/ApplicazioneRestSpring/ApplicazioneRestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package it.devlecce.ApplicazioneRestSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicazioneRestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
